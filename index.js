256a94703e5ff66ffe2937fd373e009c52389568
Priscillah Njai <priscillahnjaii@gmail.com>
2025-03-08 20:31:41 +0300

    Arithmetics complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 40;
+const num4 = 6;
+const mod = num3 % num4
+
+const numbers = [0,5,10,15,20];
+const max = Math.max(...numbers);
